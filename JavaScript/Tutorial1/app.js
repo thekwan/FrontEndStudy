05ab5cc7de5a76cb59fa54b619717432ff869eb6
deokhwan <thekwan@naver.com>
2026-06-13 11:33:45 +0900

    Router design (before)

--- JavaScript/Tutorial1/app.js
+++ JavaScript/Tutorial1/app.js
@@ -5,36 +5,37 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-
-window.addEventListener('hashchange', function() {
-    // console.log('hash is changed!');
-    // console.log(location.hash);
-    const id = location.hash.substring(1);
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+function getData(url) {
+    ajax.open('GET', url, false);
     ajax.send();
 
-    const newsConetnt = JSON.parse(ajax.response)
-    const title = document.createElement('h1');
-    // console.log(newsConetnt)
+    return JSON.parse(ajax.response);
+}
 
+const newsFeed = getData(NEWS_URL);
+
+window.addEventListener('hashchange', function() {
+    const id = location.hash.substring(1);
+    const newsConetnt = getData(CONTENT_URL.replace('@id', id));
+    const title = document.createElement('h1');
     title.innerHTML = newsConetnt.title;
+
     content.appendChild(title);
 });
 
 const ul = document.createElement('ul');
 for (let i = 0; i < 10; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    const div = document.createElement('div');
 
-    a.href = `#${newsFeed[i].id}`;
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    div.innerHTML = `
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+    `;
 
-    li.appendChild(a);
-    ul.appendChild(li);
+    ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(content);
